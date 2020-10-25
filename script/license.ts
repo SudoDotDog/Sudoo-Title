3f6b3660d39840a83f1f595799342eed79c3c5c5
WMXPY <wm@mengw.io>
2020-10-25 17:36:00 -0500

    fix depdencenis

--- script/license.ts
+++ script/license.ts
@@ -26,7 +26,5 @@ const appPackage: any = {
     license: parent.license,
     bugs: parent.bugs,
     homepage: parent.homepage,
-    dependencies: parent.dependencies,
-    peerDependencies: parent.peerDependencies,
 };
 Fs.writeFileSync(Path.join(appPath, 'package.json'), JSON.stringify(appPackage, null, 2), 'utf8');
